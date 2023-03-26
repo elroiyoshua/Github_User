d7c4be3fd6ea62fe9a1082c85b31d8f017a65153
Elroi Yoshua <elroiyoshua@student.telkomuniversity.ac.id>
2023-03-26 18:10:06 +0700

    Making Search view

--- app/src/main/java/com/example/githubuser/MainActivity.kt
+++ app/src/main/java/com/example/githubuser/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.githubuser
 
+import android.app.SearchManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.githubuser.databinding.ActivityMainBinding
@@ -18,7 +23,7 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        supportActionBar?.hide()
+        //supportActionBar?.hide()
         adapter = UserAdapter()
         //adapter.notifyDataSetChanged()
         binding.apply {
@@ -36,9 +41,9 @@ class MainActivity : AppCompatActivity() {
         binding.apply {
             if (adapter.itemCount == 0){
                 viewModel.setSearch("elroi")
+            }else{
+                adapter.notifyDataSetChanged()
             }
-//            val query :String = "elroi"
-//            viewModel.setSearch(query)
         }
         viewModel.getSearchUssers().observe(this,{
             if(it!= null){
@@ -47,6 +52,32 @@ class MainActivity : AppCompatActivity() {
         })
 
 
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.option_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                if (query ==null){
+                    Toast.makeText(this@MainActivity, "Masukan Username", Toast.LENGTH_SHORT).show()
+                    searchView.clearFocus()
+                }else{
+                    viewModel.setSearch(query)
+                    searchView.clearFocus()
+                    adapter.notifyDataSetChanged()
+                }
+                return true
+            }
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+        return true
     }
     private fun showLoading(isLoading: Boolean) {
         if (isLoading) {
